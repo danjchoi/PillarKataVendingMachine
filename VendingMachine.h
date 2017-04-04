3632efae7b4a9f0331dea3ebc40368ffeb82d423
Daniel <danjchoi@umich.edu>
2017-04-03 22:35:23 -0400

    new constructor and new private bool added

--- VendingMachine.h
+++ VendingMachine.h
@@ -4,6 +4,7 @@
 class VendingMachine{
 public:
 	VendingMachine();
+	VendingMachine(bool exactChange_in);
 	VendingMachine(bool colaSoldout_in, bool chipsSoldout_in, bool candySoldout_in);
 	std::string printDisplay();
 	void depositCoin(char coin);
@@ -16,6 +17,7 @@ private:
 	void updateDisplay();
 	void makeChange();
 	bool displayChangedByOtherMember;
+	bool exactChange;
 	bool colaSoldout;
 	bool chipsSoldout;
 	bool candySoldout;
